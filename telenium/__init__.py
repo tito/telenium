88b5cdb9ecb382b304b2c4c7925e06e2be629c6e
Mathieu Virbel <txprog@gmail.com>
2021-01-10 19:26:03 +0100

    new connection methods to install() telenium within the app, and connect() remotly easily

--- telenium/__init__.py
+++ telenium/__init__.py
@@ -1,3 +1,17 @@
 # coding=utf-8
 
-__version__ = "0.4.3dev"
+__version__ = "0.5.0dev"
+
+
+def install():
+    """Install the kivy telenium module into the kivy instance
+    """
+    from .mods.telenium_client import install as mod_install
+    return mod_install()
+
+
+def connect(host="localhost", port=9901, timeout=5):
+    from .client import TeleniumHttpClient
+    """Connect to a remote telenium kivy module
+    """
+    return TeleniumHttpClient(f"http://{host}:{port}/jsonrpc", timeout=timeout)
